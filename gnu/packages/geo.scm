d4dd600add82886ba5879f8140697fb4d4ed6d92
r0man <roman@burningswell.com>
2022-12-03 18:59:02 +0100

    gnu: gdal: Add support for lz4 and openjpeg.
    * gnu/packages/geo.scm (gdal): Add support for lz4 and openjpeg.

--- gnu/packages/geo.scm
+++ gnu/packages/geo.scm
@@ -1084,8 +1084,10 @@ development.")
            libpng
            libtiff
            libwebp
+           lz4
            netcdf
            openssl
+           openjpeg
            pcre2
            postgresql ; libpq
            proj
