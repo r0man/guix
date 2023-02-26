0b147f786b48a38c45e998359fd4ebe967932b41
r0man <roman@burningswell.com>
2023-02-26 17:59:24 +0100

    gnu: Add rust-serial-unix-0.4.
    * gnu/packages/crates-io.scm (rust-serial-unix-0.4): New variable.

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -52521,6 +52521,30 @@ that functions generically over all serial port types and to implement new
 serial port types that work with generic code.")
     (license license:expat)))
 
+(define-public rust-serial-unix-0.4
+  (package
+    (name "rust-serial-unix")
+    (version "0.4.0")
+    (source (origin
+              (method url-fetch)
+              (uri (crate-uri "serial-unix" version))
+              (file-name (string-append name "-" version ".tar.gz"))
+              (sha256
+               (base32
+                "1dyaaca8g4q5qzc2l01yirzs6igmhc9agg4w8m5f4rnqr6jbqgzh"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:skip-build? #t
+       #:cargo-inputs (("rust-ioctl-rs" ,rust-ioctl-rs-0.1)
+                       ("rust-libc" ,rust-libc-0.2)
+                       ("rust-serial-core" ,rust-serial-core-0.4)
+                       ("rust-termios" ,rust-termios-0.2))))
+    (home-page "https://github.com/dcuddeback/serial-rs")
+    (synopsis "Serial port implementation for Unix.")
+    (description "The serial-unix crate provides a serial port implementation
+for Unix operating systems.")
+    (license license:expat)))
+
 (define-public rust-serial-test-0.6
   (package
     (name "rust-serial-test")
